31d3289281ec11bcb33b91559460e8b23dbc87ef
Mariusz Klimek <tarnos7@gmail.com>
2016-01-15 23:43:44 +0100

    Added character creation screen

--- Javascript/Controllers/GameController.js
+++ Javascript/Controllers/GameController.js
@@ -4,6 +4,45 @@ dbcapp.controller(
     'GameController',
     function (
         $scope,
-        $interval
+        $interval,
+        $http
        ) {
+        $http.get('json/stats.json').success(function (data) {
+            $scope.saveList = data;
+        });
+        $http.get('json/races.json').success(function (data) {
+            $scope.raceList = data;
+        });
+        $http.get('json/masters.json').success(function (data) {
+            $scope.masterList = data;
+        });
+        $scope.player = {
+            name: "",
+            race: "",
+            gender: "",
+            master: "",
+        };
+        $scope.genderList = ['Male', 'Female']
+        $scope.gameMenu = {
+            loadingScreen: {
+                state: true,
+            },
+            startMen: {
+                state: true,
+            },
+            charSelect: {
+                state: false,
+                toggle: function () {
+                    this.state = !this.state;
+                    $scope.gameMenu.startMen.state = !this.state;
+                }
+            },
+            charCreate: {
+                state: false,
+                toggle: function () {
+                    this.state = !this.state;
+                    $scope.gameMenu.charSelect.state = !this.state;
+                }
+            }
+        };
     });

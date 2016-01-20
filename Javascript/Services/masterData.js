31e2902fbfdcddddd2ffe4821820910cb48f4f41
ArjaaAine <arjaaaine@gmail.com>
2016-01-19 21:52:13 -0800

    Fixed Json Load

--- Javascript/Services/masterData.js
+++ Javascript/Services/masterData.js
@@ -3,9 +3,15 @@
 dbcapp.factory(
     'masterData',
     function ($http) {
-        var masterList = {};
-        $http.get('json/masters.json').success(function (data) {
-            masterList = data;
+        var masterList = {
+            masters: {}
+        };
+        $http.get(
+            'json/masters.json'
+        ).success(function (data) {
+            masterList.masters = data;
+        }).error(function (error) {
+            console.log(error)
         });
         return masterList;
     });

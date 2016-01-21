3f44a3e3e2e515eab0fa9b0c2d646dd7426aaf37
Mariusz Klimek <tarnos7@gmail.com>
2016-01-21 18:32:46 +0100

    moved player stats/inventory/equipped items to services.

--- Javascript/Controllers/GameController.js
+++ Javascript/Controllers/GameController.js
@@ -5,6 +5,7 @@ dbcapp.controller(
     function (
         $scope,
         $interval,
+        playerData,
         itemData,
         raceData,
         charCreationData,
@@ -18,73 +19,9 @@ dbcapp.controller(
         $http.get('../json/stats.json').success(function (data) {
             $scope.saveList = data;
         });
-       
+        $scope.player = playerData;
         //Initial Objects
-        $scope.player = {
-            health: { name: "Life Points", value: 10, description: 'Life Force, if it becomes 0, you die' },
-            maxHealth: { name: "Max Life Points", value: 10 },
-            healthPerCent: function(){
-                return this.health.value / this.maxHealth.value * 100;
-            },
-            energy: { name: "Ki Points", value: 10, description: 'Energy, required to use various techniques' },
-            maxEnergy: { name: "Max Ki Points", value: 10 },
-            energyPercent: function () {
-                return this.energy.value / this.maxEnergy.value * 100;
-            },
-            baseStats: {
-                strength: { name: "Strength", value: 1, description: 'Increase physical damage' },
-                speed: { name: "Speed", value: 1, description: 'Evasion, turn order' },
-                persistence: { name: "Persistence", value: 1, description: 'Decrease damage taken' },
-                intelligence: { name: "Intelligence", value: 1, description: 'Training Speed' },
-            },
-            name: "",
-            race: {
-                Stats: {
-                    strength: 0,
-                    speed: 0,
-                    persistence: 0,
-                    intelligence:0
-                }
-            },
-            equipStats:{
-                strength: function () {
-                    var total = equipStatTotal('strength', $scope.equippedItems);
-                    return total;
-                },
-                speed: function () {
-                    var total = equipStatTotal('speed', $scope.equippedItems);
-                    return total;
-                },
-                persistence: function () {
-                    var total = equipStatTotal('persistence', $scope.equippedItems);
-                    return total;
-                },
-                intelligence: function () {
-                    var total = equipStatTotal('intelligence', $scope.equippedItems);
-                    return total;
-                },
-            },
-            totalStats: {
-                strength: function () {
-                    return $scope.player.baseStats.strength.value + $scope.player.race.Stats.strength + $scope.player.equipStats.strength();
-                },
-                speed: function () {
-                    return $scope.player.baseStats.speed.value + $scope.player.race.Stats.speed + $scope.player.equipStats.speed();
-                },
-                persistence: function () {
-                    return $scope.player.baseStats.persistence.value + $scope.player.race.Stats.persistence + $scope.player.equipStats.persistence();
-                },
-                intelligence: function () {
-                    return $scope.player.baseStats.intelligence.value + $scope.player.race.Stats.intelligence + $scope.player.equipStats.intelligence();
-                },
-
-            },
-            gender: "",
-            master: "",
-            charType: {},
-            inventorySlot: 30,
-            itemId:0,
-        };
+       
         $scope.random = function () {
             var randomHp = Math.floor(Math.random() * 10 + 1);
             var randomEnergy = Math.floor(Math.random() * 10 + 1);
@@ -99,16 +36,7 @@ dbcapp.controller(
             { title: 'Map', content: 'Views/map.html', disabled: false }
         ];
         //Initialize player inventory
-        $scope.playerInventory = [];
-        $scope.equippedItems = {
-            helmet: {},
-            weapon: {},
-            chest: {},
-            offHand: {},
-            wrist: {},
-            boots: {},
-            special: {}
-        };
+        
         
         $scope.gameMenu = {
             loadingScreen: {
@@ -159,26 +87,26 @@ dbcapp.controller(
             var id = $scope.player.itemId;
             var item = addItem(itemDataInv, id);
             $scope.player.itemId++;
-            $scope.playerInventory.push(item);
+            $scope.player.inventory.push(item);
         }
         $scope.unequip = function (type, id) {
-            var item = $scope.equippedItems[type];
+            var item = $scope.player.equippedItems[type];
             item.isEquipped = false;
-            $scope.playerInventory.push(item);
-            $scope.equippedItems[type] = {};
+            $scope.player.inventory.push(item);
+            $scope.player.equippedItems[type] = {};
         }
         $scope.equip = function (type, id) {
-            var inventoryObj = $scope.playerInventory;
+            var inventoryObj = $scope.player.inventory;
             var item = filterItemId(inventoryObj, id);
-            var equippedItem = $scope.equippedItems[type];
+            var equippedItem = $scope.player.equippedItems[type];
             if (equippedItem.isEquipped === true) {
                 $scope.unequip(equippedItem.itemType, equippedItem.id);
             };
             item.isEquipped = true;
-            $scope.equippedItems[type] = item;
-            var index = $scope.playerInventory.indexOf(item, 0);
+            $scope.player.equippedItems[type] = item;
+            var index = $scope.player.inventory.indexOf(item, 0);
             if (index > -1) {
-                $scope.playerInventory.splice(index, 1);
+                $scope.player.inventory.splice(index, 1);
             };
         }
 
--- Javascript/Services/playerData.js
+++ Javascript/Services/playerData.js
@@ -3,9 +3,81 @@
 dbcapp.factory(
     'playerData',
     function ($http) {
-
+        
         var player = {
-            stats: {}
+            inventory: [],
+            equippedItems: {
+                helmet: {},
+                weapon: {},
+                chest: {},
+                offHand: {},
+                wrist: {},
+                boots: {},
+                special: {}
+            },
+            health: { name: "Life Points", value: 10, description: 'Life Force, if it becomes 0, you die' },
+            maxHealth: { name: "Max Life Points", value: 10 },
+            healthPerCent: function () {
+                return this.health.value / this.maxHealth.value * 100;
+            },
+            energy: { name: "Ki Points", value: 10, description: 'Energy, required to use various techniques' },
+            maxEnergy: { name: "Max Ki Points", value: 10 },
+            energyPercent: function () {
+                return this.energy.value / this.maxEnergy.value * 100;
+            },
+            baseStats: {
+                strength: { name: "Strength", value: 1, description: 'Increase physical damage' },
+                speed: { name: "Speed", value: 1, description: 'Evasion, turn order' },
+                persistence: { name: "Persistence", value: 1, description: 'Decrease damage taken' },
+                intelligence: { name: "Intelligence", value: 1, description: 'Training Speed' },
+            },
+            name: "",
+            race: {
+                Stats: {
+                    strength: 0,
+                    speed: 0,
+                    persistence: 0,
+                    intelligence: 0
+                }
+            },
+            equipStats: {
+                strength: function () {
+                    var total = equipStatTotal('strength', player.equippedItems);
+                    return total;
+                },
+                speed: function () {
+                    var total = equipStatTotal('speed', player.equippedItems);
+                    return total;
+                },
+                persistence: function () {
+                    var total = equipStatTotal('persistence', player.equippedItems);
+                    return total;
+                },
+                intelligence: function () {
+                    var total = equipStatTotal('intelligence', player.equippedItems);
+                    return total;
+                },
+            },
+            totalStats: {
+                strength: function () {
+                    return player.baseStats.strength.value + player.race.Stats.strength + player.equipStats.strength();
+                },
+                speed: function () {
+                    return player.baseStats.speed.value + player.race.Stats.speed + player.equipStats.speed();
+                },
+                persistence: function () {
+                    return player.baseStats.persistence.value + player.race.Stats.persistence + player.equipStats.persistence();
+                },
+                intelligence: function () {
+                    return player.baseStats.intelligence.value + player.race.Stats.intelligence + player.equipStats.intelligence();
+                },
+
+            },
+            gender: "",
+            master: "",
+            charType: {},
+            inventorySlot: 30,
+            itemId: 0,
         };
 
 
